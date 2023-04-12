03681863119717629c6e4b63c4805523b866c344
manalirupapara <rupaparadishant2@gmail.com>
2023-04-12 16:30:00 +0530

    ch-1

---
+++ CH1-2.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("name\t\t:manali\n");
+	printf("age\t\t:18\n");
+	printf("contact no\t:9328079519\n");
+
+
+
+	getch();
+}
---
+++ ch1-1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("hello c lauguage!!\n");
+	printf("i am manali\n");
+
+
+	getch();
+
+}
