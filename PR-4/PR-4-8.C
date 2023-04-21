6d33acdfc7e8119132e8ebf82f1b379da8a5bced
manalirupapara <rupaparadishant2@gmail.com>
2023-04-21 18:41:06 +0530

    pr-4

---
+++ PR-4/PR-4-8.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+	clrscr();
+	for(i=45;i<=49;i++)
+	{
+		for(j=45;j<=i;j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+
+
+	getch();
+
+}
+
